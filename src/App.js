3949a6bcb8c4ced1954209b165fc87ed9460953a
jroberto-inv <jroberto@inv.com.vc>
2022-04-19 20:42:34 -0300

    create component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      teste
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch
+            size={25}
+            color="#fff"
+          />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 12313123</h2>
+        <span>Rua teste rua</span>
+        <span>Complemento: complemento teste</span>
+        <span>Vila Teste</span>
+        <span>Ribeirão Pires - SP</span>
+      </main>
+
     </div>
   );
 }
